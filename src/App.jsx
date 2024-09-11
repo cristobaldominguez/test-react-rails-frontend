6eccee8e89c290ca3bc82bb87f8ab32208ba9afa
Cristobal Dominguez <cristobald@gmail.com>
2024-09-11 11:01:01 +0200

    Adds Routes and Navigation Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Navigation from './components/Navigation/index.jsx'
 function App() {
   return <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
+    <Navigation />
+    <Routes>
+    </Routes>
 }
 
 export default App
