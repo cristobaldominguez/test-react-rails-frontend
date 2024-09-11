9accda3b27032188a4de20ca55280dccd15894fc
Cristobal Dominguez <cristobald@gmail.com>
2024-09-11 11:03:07 +0200

    Adds Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Navigation from './components/Navigation/index.jsx'
+
+// Pages
+import Home from './pages/Home/index.jsx'
+import Login from './pages/Login/index.jsx'
+import NotFound from './pages/NotFound/index.jsx'
+import Registration from './pages/Registration/index.jsx'
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  return <>
     <Navigation />
+    <h1 className="text-3xl font-bold underline">Mini Twitter</h1>
     <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/registration' element={<Registration />} />
+      <Route path='/login' element={<Login />} />
+
+
+      <Route path='*' element={<NotFound />} />
     </Routes>
+  </>
 }
 
 export default App
